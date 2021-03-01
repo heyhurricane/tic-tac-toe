4bdbd6bff86dacd49c434a20b8fffeeffdb618b2
heyhurricane <dinohka@mail.ru>
2021-03-01 21:46:58 +0400

    tic tac toe is done

---
+++ main.js
@@ -0,0 +1,81 @@
+const square = document.querySelector('.square');
+let isCross = true;
+let count = 0;
+const winSteps = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+];
+
+let endOfGame = false;
+
+const reload = () => {
+  const all = square.querySelectorAll('.btn');
+  all.forEach((item) => {
+    item.classList.remove('btn-x');
+    item.classList.remove('btn-o');
+    endOfGame = false;
+    isCross = true;
+    count = 0;
+  });
+}
+
+const isEnd = (player) => {
+  let allSteps = [];
+  let winner;
+  if (player) {
+    const allX = square.querySelectorAll('.btn-x');
+    winner = 'Крестики';
+    if (allX.length > 2) {
+      allX.forEach((item) => {
+        allSteps.push(item.id);
+      });  
+    }
+  }
+  else {
+    winner = 'Нолики';
+    const allO = square.querySelectorAll('.btn-o');
+    if (allO.length > 2) {
+      allO.forEach((item) => {
+        allSteps.push(item.id);
+      }); 
+    }
+  } 
+  if (allSteps.length > 2) {
+    for (let k = 0; k < winSteps.length; k++) {
+      let countWinStep = 0;
+      for (let i = 0; i < allSteps.length; i++) {
+        for (let j = 0; j < winSteps[k].length; j++) {
+          if (winSteps[k][j] == allSteps[i]) { countWinStep++; break;}
+        }
+      }
+      if (countWinStep === 3) { 
+        alert(winner + ' победили!'); 
+        endOfGame = true; break; }
+      if (countWinStep !== 3 && count === 9 && k === winSteps.length - 1) { 
+        alert('Ничья'); 
+        endOfGame = true; break; }
+    }
+  }
+}
+
+
+square.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('btn') && !target.classList.contains('btn-x') && !target.classList.contains('btn-o')) {
+    if (isCross) {
+      target.classList.add('btn-x');
+    }
+    else {
+      target.classList.add('btn-o');
+    }
+    setTimeout(() => {
+      count++;
+      if (count > 2) {
+        isEnd(isCross);
+      }
+      if (endOfGame) { 
+        reload();
+      }
+      else { isCross = !isCross; }
+    }, 100);
+  }
+});
